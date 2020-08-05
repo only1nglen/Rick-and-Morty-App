625b9075819c26473c5102fb2abfc5e473bf8fc8
only1nglen <only1nglen@gmail.com>
2020-08-05 14:45:36 -0400

    Updates

---
+++ precache-manifest.1c0cfb06aec3db65e2b40c42893567a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e04b6cc008b3444864b965da72961e",
+    "url": "/Rick-and-Morty-App/index.html"
+  },
+  {
+    "revision": "b8af028629be9a65890f",
+    "url": "/Rick-and-Morty-App/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "75ca6d841ca96d3b8f2e",
+    "url": "/Rick-and-Morty-App/static/js/2.6d34507a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Rick-and-Morty-App/static/js/2.6d34507a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8af028629be9a65890f",
+    "url": "/Rick-and-Morty-App/static/js/main.4cf06a72.chunk.js"
+  },
+  {
+    "revision": "692b02e48f18d3c5916c",
+    "url": "/Rick-and-Morty-App/static/js/runtime-main.9ce03b6c.js"
+  }
+]);
