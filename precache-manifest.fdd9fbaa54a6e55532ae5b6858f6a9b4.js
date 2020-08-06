22778f9caa0e960f9d77945beea3d91c073a94ec
only1nglen <only1nglen@gmail.com>
2020-08-06 10:43:52 -0400

    Updates

---
+++ precache-manifest.fdd9fbaa54a6e55532ae5b6858f6a9b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d8ba61d17ae3a559afdaa14840efd8",
+    "url": "/Rick-and-Morty-App/index.html"
+  },
+  {
+    "revision": "54e89f8b1359207612af",
+    "url": "/Rick-and-Morty-App/static/css/main.611ab4ba.chunk.css"
+  },
+  {
+    "revision": "2a84deb939ef0f87b833",
+    "url": "/Rick-and-Morty-App/static/js/2.ec8f167c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Rick-and-Morty-App/static/js/2.ec8f167c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54e89f8b1359207612af",
+    "url": "/Rick-and-Morty-App/static/js/main.e5eda86d.chunk.js"
+  },
+  {
+    "revision": "692b02e48f18d3c5916c",
+    "url": "/Rick-and-Morty-App/static/js/runtime-main.9ce03b6c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Rick-and-Morty-App/precache-manifest.e097a0f3d5bee1b4d648007651b2c86b.js"
+  "/Rick-and-Morty-App/precache-manifest.fdd9fbaa54a6e55532ae5b6858f6a9b4.js"
 );
 
 self.addEventListener('message', (event) => {
