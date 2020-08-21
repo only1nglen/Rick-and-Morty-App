4568969c8ba9bdaefab5b6af5b9c457f45d02d85
only1nglen <only1nglen@gmail.com>
2020-08-21 18:18:38 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Rick-and-Morty-App/precache-manifest.3954c02f8abe8c4955c6927937bedc66.js"
+  "/Rick-and-Morty-App/precache-manifest.5b7781469f71c48dcae41d0d5aaf0da7.js"
 );
 
 self.addEventListener('message', (event) => {
